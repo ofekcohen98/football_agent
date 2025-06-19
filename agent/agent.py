fcd9a285c7bb4c520c03008e91b42775fbf2100f
ofekcohen98 <77544607+ofekcohen98@users.noreply.github.com>
2025-06-19 11:21:42 +0300

    Add basic agent config

---
+++ agent/agent.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(
+  api_key=api_key
+)
+
+completion = client.chat.completions.create(
+  model="gpt-4o-mini",
+  store=True,
+  messages=[
+    {"role": "user", "content": "Tell me a joke about cats."},
+  ]
+)
+
+print(completion.choices[0].message);
